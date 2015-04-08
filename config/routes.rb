fd76d52a25a9f27974026d65cb4f6cc36b8868a9
khuongnt1688 <adayinthelife1988@gmail.com>
2015-04-08 17:26:58 +0700

    create static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'help' => 'static_pages#help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
