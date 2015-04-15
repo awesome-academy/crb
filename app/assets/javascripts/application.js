efb0f94d59f249d3a9855886207908bd2624c227
Nguyen Binh Dieu <nguyen.binh.dieu@framgia.com>
2015-04-15 17:05:13 +0700

    Change gem and reconfig datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,56 +17,7 @@
 //= require bootstrap
 //= require moment
 //= require fullcalendar
-//= require bootstrap-datepicker
+//= require bootstrap-datetimepicker
 //= require websocket_rails/main
-
-$(document).ready(function() {
-  $('#calendar').fullCalendar({
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    defaultView: 'month',
-    defaultDate: new Date(),
-    editable: true,
-    eventLimit: true,
-    weekends: false,
-    height: $(window).height() - $("header").height() - $("footer").height() - 100,
-    minTime: "08:00:00",
-    maxTime: "17:00:00",
-    allDaySlot: false,
-    editable: true,
-    eventLimit: true,
-    events: function(start, end, timezone, callback) {
-      $.ajax({
-        url: '/schedules',
-        type: 'GET',
-        success: function(doc) {
-          var events = [];
-          if(doc.schedules){
-            $.map(doc.schedules, function(schedule) {
-              events.push({
-                id: schedule.id,
-                title: schedule.title,
-                start: schedule.start_time,
-                end: schedule.finish_time
-              });
-            });
-          }
-          callback(events);
-        }
-      });
-    }
-  });
-
-  $('#datepicker').datepicker({
-    format: 'yyyy-mm-dd',
-    autoclose: true,
-    clearBtn: true,
-    daysOfWeekDisabled: [0,6],
-    todayBtn: true,
-    todayHighlight: true,
-    startDate: 'Beginning of time'
-  });
-});
+//= require select2
+//= require custom
---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    defaultView: 'month',
+    defaultDate: new Date(),
+    editable: true,
+    eventLimit: true,
+    weekends: false,
+    height: $(window).height() - $("header").height() - $("footer").height() - 100,
+    minTime: "08:00:00",
+    maxTime: "17:00:00",
+    allDaySlot: false,
+    editable: true,
+    eventLimit: true,
+    events: function(start, end, timezone, callback) {
+      $.ajax({
+        url: '/schedules',
+        type: 'GET',
+        success: function(doc) {
+          var events = [];
+          if(doc.schedules){
+            $.map(doc.schedules, function(schedule) {
+              events.push({
+                id: schedule.id,
+                title: schedule.title,
+                start: schedule.start_time,
+                end: schedule.finish_time
+              });
+            });
+          }
+          callback(events);
+        }
+      });
+    }
+  });
+
+  $('#datepicker').datetimepicker({
+    format: 'LLL',
+    daysOfWeekDisabled: [0, 6],
+    dayViewHeaderFormat: 'DD - MM - YYYY'
+  });
+  $('.select2').select2();
+});
