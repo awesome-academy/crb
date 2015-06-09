2b477c4f7e44b2ab07404ec9b4f11b144b8f8d05
Nguyen Ngoc Truong <nguyen.ngoc.truong@framgia.com>
2015-06-09 08:18:34 +0700

    load my schedule

--- app/controllers/api/my_schedules_controller.rb
+++ app/controllers/api/my_schedules_controller.rb
@@ -1,7 +1,8 @@
 class Api::MySchedulesController < ApplicationController
   def index
-    @schedules = Schedule.my_schedule(current_user.id)
-                  .page(params[:my_page]).per_page(Settings.per_page).order_start_time
+    @schedules = Schedule.my_schedule(current_user.id).order_start_time
+                  .paginate page:params[:my_page], per_page: Settings.per_page,
+                  total_entries: Settings.number_of_schedules
 
     if params[:my_page]
       respond_to do |format|
