730f96ee307ab06241e380d9bfab951cb60d2cd0
Tung Pham <phamvantung283@gmail.com>
2015-06-09 14:42:22 +0700

    create layout for fullcalendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+  layout "fullcalendar"
   before_action :authenticate_user!
+
   def home
     @rooms = Room.pluck :name, :id
     @schedule = Schedule.new
