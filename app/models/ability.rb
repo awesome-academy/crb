bcd852fad06f9228b099c135cee6390c081aba67
Nguyen Ngoc Truong <nguyen.ngoc.truong@framgia.com>
2015-06-22 16:29:18 +0700

    fix button edit on show schedule and update cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,21 @@ class Ability
 
     case
     when user.admin?
-      can :manage, :all
+      can :manage, User
+      can [:create, :read, :destroy], Schedule
+      can [:update], Schedule do |schedule|
+        schedule.finish_time > Time.zone.now
+      end
+      can :manage, Repeat
     when user.normal?
       can [:read, :update], User, id: user.id
       can [:create, :read], Schedule
-      can [:update, :destroy], Schedule, user_id: user.id
+      can [:destroy], Schedule, user_id: user.id
       can :destroy, Repeat, user_id: user.id
+      can [:update], Schedule do |schedule|
+        schedule.user_id == user.id && schedule.finish_time > Time.zone.now
+      end
     end
+
   end
 end
