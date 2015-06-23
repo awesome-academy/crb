4baf4b0229b057ac0f67169412451e5d3890db44
Nguyen Ngoc Truong <nguyen.ngoc.truong@framgia.com>
2015-06-23 09:11:21 +0700

    fix cancancan for rails admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,16 +10,16 @@ class Ability
       can :dashboard
       can :manage, User
       can [:create, :read, :destroy], Schedule
-      can [:update], Schedule do |schedule|
+      can :update, Schedule do |schedule|
         schedule.finish_time > Time.zone.now
       end
       can :manage, Repeat
     when user.normal?
       can [:read, :update], User, id: user.id
       can [:create, :read], Schedule
-      can [:destroy], Schedule, user_id: user.id
+      can :destroy, Schedule, user_id: user.id
       can :destroy, Repeat, user_id: user.id
-      can [:update], Schedule do |schedule|
+      can :update, Schedule do |schedule|
         schedule.user_id == user.id && schedule.finish_time > Time.zone.now
       end
     end
