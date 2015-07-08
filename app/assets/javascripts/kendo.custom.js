ce52bd1a204ccdaee4da64ee4fa2e2b7084d605e
Lim Kimhuor <kimhuorlim@gmail.com>
2015-07-08 16:52:11 +0700

    Timepicker around 7h to 22h

--- app/assets/javascripts/kendo.custom.js
+++ app/assets/javascripts/kendo.custom.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   var now_in_milisec = (new Date()).getTime();
+  var hour = (new Date()).getHours();
+  var minute = (new Date()).getMinutes();
+  var second = (new Date()).getSeconds();
   var interval = 15;
   var interval_in_milisec = interval * 60 * 1000;
-  var min_in_milisec = (parseInt(now_in_milisec / interval_in_milisec) + 1) * interval_in_milisec
+  var min_in_milisec = (parseInt(now_in_milisec / interval_in_milisec) + 1) * interval_in_milisec;
+  var tenPM = new Date(now_in_milisec - (hour * 60 * 60 + minute * 60 + second) * 1000 + 22*3600*1000);
 
   var startChange = function startChange() {
     var startDate = schedule_start_time.value();
@@ -62,6 +66,11 @@ $(document).ready(function() {
     },
   }).data("kendoDateTimePicker");
 
+  var start_timepicker = $("#schedule_start_time").data("kendoDateTimePicker");
+  start_timepicker.setOptions({
+    max: new Date(tenPM)
+  });
+
   schedule_finish_time = $("#schedule_finish_time").kendoDateTimePicker({
     min: new Date(min_in_milisec + interval_in_milisec),
     interval: interval,
@@ -86,9 +95,15 @@ $(document).ready(function() {
     },
   }).data("kendoDateTimePicker");
 
+  var finish_timepicker = $("#schedule_finish_time").data("kendoDateTimePicker");
+  finish_timepicker.setOptions({
+    max: new Date(tenPM)
+  });
+
   schedule_start_time.max(schedule_finish_time.value());
   schedule_finish_time.min(schedule_start_time.value());
 
   $("#schedule_start_time").attr("readonly", "readonly");
   $("#schedule_finish_time").attr("readonly", "readonly");
-})
+
+});
