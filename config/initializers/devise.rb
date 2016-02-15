9d92068836a9e561e9dc30484848fb0ea8f389cd
truonglocbinh <truonglocbinh93@gmail.com>
2016-02-15 14:10:17 +0700

    fix google_synch

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], {
+  config.omniauth :google_oauth2, ENV["GOOGLE_CALENDAR_KEY"], ENV["GOOGLE_CALENDAR_SECRET"], {
     access_type: "offline",
     scope: "email, https://www.googleapis.com/auth/calendar"}
 end
